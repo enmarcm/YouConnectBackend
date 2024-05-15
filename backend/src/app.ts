b748595dfbc61369075e53450bb30c9d03cae850
enmarcm <theenmanuel123@gmail.com>
2024-05-15 01:01:14 -0400

    midNotfound

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { PORT } from "./constants";
 import { startServer } from "./functions";
-import 'dotenv/config'
-import { midCors, midJson, midNotJson } from "./middlewares/middlewares";
+import "dotenv/config";
+import {
+  midCors,
+  midJson,
+  midNotFound,
+  midNotJson,
+} from "./middlewares/middlewares";
 
 const app = express();
 app.use(midJson());
-app.use(midCors())
-app.use(midNotJson)
+app.use(midCors());
+app.use(midNotJson);
 
-startServer({app, PORT});
+//{ Aqui iran las rutas
+
+app.use(midNotFound);
+
+startServer({ app, PORT });
--- backend/src/middlewares/middlewares.ts
+++ backend/src/middlewares/middlewares.ts
@@ -1,9 +1,12 @@
 import cors from "cors";
 import express from "express";
 import midNotJson from "./midNotJson";
+import midNotFound from "./midNotFound";
 
 export const midJson = () => express.json();
 
 export const midCors = () => cors({ credentials: true, origin: true });
 
 export { midNotJson };
+
+export { midNotFound };
