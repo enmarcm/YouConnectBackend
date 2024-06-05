456ebeb8087c55457b4c13f7f480af9d518511d1
enmarcm <theenmanuel123@gmail.com>
2024-06-05 13:41:32 -0400

    changes

--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,7 @@ import { StartServerProps } from "./types";
 import picocolors from "picocolors";
 
 export function startServer({ app, PORT }: StartServerProps) {
-  app.listen(PORT, "192.168.3.106", () => {
+  app.listen(PORT, () => {
     console.log(
       picocolors.bgBlack(picocolors.green(`SERVER RUNNING ON PORT ${PORT}`))
     );
