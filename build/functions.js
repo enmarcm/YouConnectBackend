5529d5d7d9bb3214db98835eb772eb9f644e007e
enmarcm <theenmanuel123@gmail.com>
2024-06-08 19:49:22 -0400

    changes

--- build/functions.js
+++ build/functions.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.startServer = void 0;
 const picocolors_1 = __importDefault(require("picocolors"));
 function startServer({ app, PORT }) {
-    app.listen(PORT, '192.168.109.126', () => {
+    app.listen(PORT, () => {
         console.log(picocolors_1.default.bgBlack(picocolors_1.default.green(`SERVER RUNNING ON PORT ${PORT}`)));
     });
     return;
--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,7 @@ import { StartServerProps } from "./types";
 import picocolors from "picocolors";
 
 export function startServer({ app, PORT }: StartServerProps) {
-  app.listen(PORT,'192.168.109.126',  () => {
+  app.listen(PORT, () => {
     console.log(
       picocolors.bgBlack(picocolors.green(`SERVER RUNNING ON PORT ${PORT}`))
     );
